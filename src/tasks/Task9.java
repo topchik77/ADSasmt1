d2cd9d8f646b77935983316f9eb89b5e358ca643
topchik77 <adilvotalob@gmail.com>
2024-04-07 23:22:28 +0500

    task9

--- src/tasks/Task9.java
+++ src/tasks/Task9.java
@@ -1,7 +1,23 @@
 package tasks;
 
+import java.util.Scanner;
+
 public class Task9 {
     public void runTask(){
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("enter n ");
+        int n = scanner.nextInt();
+        System.out.print("enter k ");
+        int k = scanner.nextInt();
+
+        System.out.println("C(" +n+ ", "+k + ") = " + binCoef(n, k));
+        scanner.close();
+    }
+    private static int binCoef(int n, int k){
+        if(k==0 || k==n){
+            return 1;
+        }
+        return binCoef(n-1,k-1)+ binCoef(n-1,k);
     }
 }
