e12e6c3a802166aa5de6bfb84e35278529e96606
topchik77 <adilvotalob@gmail.com>
2024-04-07 23:46:44 +0500

    task10 and add continue running main

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,61 @@
 import java.util.Scanner;
 import tasks.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of the task to run");
-        int taskNumber = scanner.nextInt();
+        int continuerun = 1;
 
-        switch (taskNumber) {
-            case 1:
-                Task1 task1 = new Task1();
-                task1.runTask();
-                break;
-            case 2:
-                 Task2 task2 = new Task2();
-                task2.runTask();
-                break;
-            case 3:
-                Task3 task3 = new Task3();
-                task3.runTask();
-                break;
-            case 4:
-                Task4 task4 = new Task4();
-                task4.runTask();
-                break;
-            case 5:
-                Task5 task5 = new Task5();
-                task5.runTask();
-                break;
-            case 6:
-                Task6 task6 = new Task6();
-                task6.runTask();
-                break;
-            case 7:
-                Task7 task7 = new Task7();
-                task7.runTask();
-                break;
-            case 8:
-                Task8 task8 = new Task8();
-                task8.runTask();
-                break;
-            case 9:
-                Task9 task9 = new Task9();
-                task9.runTask();
-                break;
-            case 10:
-                Task10 task10 = new Task10();
-                task10.runTask();
-                break;
-            default:
-                System.out.println("enter task number from 1-10");
+        while (continuerun == 1) {
+            System.out.println("Enter the number of the task to run");
+            int taskNumber = scanner.nextInt();
+
+            switch (taskNumber) {
+                case 1:
+                    Task1 task1 = new Task1();
+                    task1.runTask();
+                    break;
+                case 2:
+                    Task2 task2 = new Task2();
+                    task2.runTask();
+                    break;
+                case 3:
+                    Task3 task3 = new Task3();
+                    task3.runTask();
+                    break;
+                case 4:
+                    Task4 task4 = new Task4();
+                    task4.runTask();
+                    break;
+                case 5:
+                    Task5 task5 = new Task5();
+                    task5.runTask();
+                    break;
+                case 6:
+                    Task6 task6 = new Task6();
+                    task6.runTask();
+                    break;
+                case 7:
+                    Task7 task7 = new Task7();
+                    task7.runTask();
+                    break;
+                case 8:
+                    Task8 task8 = new Task8();
+                    task8.runTask();
+                    break;
+                case 9:
+                    Task9 task9 = new Task9();
+                    task9.runTask();
+                    break;
+                case 10:
+                    Task10 task10 = new Task10();
+                    task10.runTask();
+                    break;
+                default:
+                    System.out.println("enter number 1-10.");
+            }
+
+            System.out.println("do you want to finish or continue  program?");
+            continuerun = scanner.nextInt();
         }
         scanner.close();
     }
