da7359e986f928449153d397a083271c971b1531
topchik77 <adilvotalob@gmail.com>
2024-04-07 23:36:02 +0500

    task10

--- src/tasks/Task10.java
+++ src/tasks/Task10.java
@@ -1,7 +1,23 @@
 package tasks;
 
-public class Task10 {
-    public void runTask(){
+import java.util.Scanner;
 
+public class Task10 {
+    public void runTask() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Greatest common divisor calculator ");
+        System.out.print("enter a ");
+        int a = scanner.nextInt();
+        System.out.print("enter b ");
+        int b = scanner.nextInt();
+        System.out.println("GCD(" + a + "," + b + ")= " + GCD(a, b));
+    }
+
+    private static int GCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return GCD(b, a % b);
+        }
     }
 }
