7c54caad386b40962a8064b256d096b337390c91
topchik77 <adilvotalob@gmail.com>
2024-04-07 14:09:08 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -12,40 +12,40 @@ public class Main {
                 task1.runTask();
                 break;
             case 2:
-                Task1 task1 = new Task1();
-                task1.runTask();
+                Task2 task2 = new Task2();
+                task2.runTask();
                 break;
             case 3:
-                Task1 task1 = new Task1();
-                task1.runTask();
+                Task3 task3 = new Task3();
+                task3.runTask();
                 break;
             case 4:
-                Task1 task1 = new Task1();
-                task1.runTask();
+                Task4 task4 = new Task4();
+                task4.runTask();
                 break;
             case 5:
-                Task1 task1 = new Task1();
-                task1.runTask();
+                Task5 task5 = new Task5();
+                task5.runTask();
                 break;
             case 6:
-                Task1 task1 = new Task1();
-                task1.runTask();
+                Task6 task6 = new Task6();
+                task6.runTask();
                 break;
             case 7:
-                Task1 task1 = new Task1();
-                task1.runTask();
+                Task7 task7 = new Task7();
+                task7.runTask();
                 break;
             case 8:
-                Task1 task1 = new Task1();
-                task1.runTask();
+                Task8 task8 = new Task8();
+                task8.runTask();
                 break;
             case 9:
-                Task1 task1 = new Task1();
-                task1.runTask();
+                Task9 task9 = new Task9();
+                task9.runTask();
                 break;
             case 10:
-                Task1 task1 = new Task1();
-                task1.runTask();
+                Task10 task10 = new Task10();
+                task10.runTask();
                 break;
             default:
                 System.out.println("Wrong task number");
--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -1,4 +1,26 @@
 package tasks;
 
+import java.util.Scanner;
+
 public class Task1 {
+    public void runTask() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("number of elements :");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int min = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("min value is" + min);
+        scanner.close();
+    }
 }
